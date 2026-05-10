e7303388f1904fde95dd75c4404050a743e0a7a1
Kutay Keleş <keleskutay61@gmail.com>
2026-05-10 15:32:59 +0300

    fix: remove unused imports and fix sorting once and for all

--- tests/test_chunker.py
+++ tests/test_chunker.py
@@ -1,9 +1,9 @@
 import os
 import sys
 
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
 from chunker import Dograyici
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 def test_ayni_metne_ayni_barkod_uretilmeli():
     dograyici = Dograyici()
